cfcfd2f0c800c7ab744e4da7b66fc3506409b63f
rainstone99 <kjdfjf@gmail.com>
2025-09-14 17:22:53 +0900

    오브젝트 랜덤 이동 구현

--- Game/Assets/Script/UnitScript.cs
+++ Game/Assets/Script/UnitScript.cs
@@ -1,20 +1,36 @@
+using System.Collections;
 using Unity.Android.Gradle.Manifest;
 using UnityEngine;
 
 public class UnitScript : MonoBehaviour
 {
-    Vector2 unitMove = new Vector2(0, 0);
+    Vector2 objectPosition;
 
-
+    void Start()
+    {
+        StartCoroutine("randomMove");
+    }
     void FixedUpdate()
     {
-        
+
     }
     // Update is called once per frame
     void Update()
     {
         transform.position =
-        Vector2.MoveTowards(transform.position, unitMove, 2f * Time.deltaTime);
-        
+        Vector2.MoveTowards(transform.position, objectPosition, 2f * Time.deltaTime);
+
+    }
+    IEnumerator randomMove()
+    {
+        while (true)
+        {
+            float randomX = Random.Range(-2.3f, +2.3f);
+            float randomY = Random.Range(-4.7f, 3.3f);
+            objectPosition = new Vector2(randomX, randomY);
+
+            float randomTime = Random.Range(1f, 7f);
+            yield return new WaitForSeconds(randomTime);       
+        }
     }
 }
