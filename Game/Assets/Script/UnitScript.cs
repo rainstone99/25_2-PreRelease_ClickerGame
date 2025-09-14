b70cc71d466bb0690783371b1c95eea1fd3ce439
rainstone99 <kjdfjf@gmail.com>
2025-09-14 22:15:00 +0900

    유닛 생성 테스트 중 버그 발생 왜 리스트 초기화가 안될까, 같은 문제 저번에 겪었는데, 뭐였더라

--- Game/Assets/Script/UnitScript.cs
+++ Game/Assets/Script/UnitScript.cs
@@ -1,24 +1,18 @@
 using System.Collections;
-using Unity.Android.Gradle.Manifest;
 using UnityEngine;
 
 public class UnitScript : MonoBehaviour
 {
     Vector2 objectPosition;
-
+    float moveSpeed;
     void Start()
     {
         StartCoroutine("randomMove");
     }
-    void FixedUpdate()
-    {
-
-    }
-    // Update is called once per frame
     void Update()
     {
         transform.position =
-        Vector2.MoveTowards(transform.position, objectPosition, 2f * Time.deltaTime);
+        Vector2.MoveTowards(transform.position, objectPosition, moveSpeed * Time.deltaTime);
 
     }
     IEnumerator randomMove()
@@ -29,6 +23,7 @@ public class UnitScript : MonoBehaviour
             float randomY = Random.Range(-4.7f, 3.3f);
             objectPosition = new Vector2(randomX, randomY);
 
+            moveSpeed = Random.Range(1f, 3f);
             float randomTime = Random.Range(1f, 7f);
             yield return new WaitForSeconds(randomTime);       
         }
